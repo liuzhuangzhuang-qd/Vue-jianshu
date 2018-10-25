bd87174419e6b453fdb5197263e899b0bbc9dba4
liuzhuangzhuang <liuzhuangzhuang_qd@caxins.com>
2018-10-25 17:44:53 +0800

    1025

---
+++ src/common/header/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import {
+    HeaderWrapper,
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button,
+    SearchWrapper
+} from "./style";
+
+class Header extends  Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            focused: false,
+        }
+        this.handleInputFocus = this.handleInputFocus.bind(this)
+        this.handleInputBlur = this.handleInputBlur.bind(this)
+    }
+    render () {
+        return (
+            <HeaderWrapper>
+                <Logo href='/'/>
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载APP</NavItem>
+                    <NavItem className='right'>登录</NavItem>
+                    <NavItem className='right'>
+                        <i className='iconfont'>&#xe636;</i>
+                    </NavItem>
+                    <SearchWrapper>
+                        <NavSearch
+                        className={ this.state.focused ? 'focused' : '' }
+                        onFocus={this.handleInputFocus}
+                        onBlur={this.handleInputBlur}
+                        ></NavSearch>
+                        <i className={ this.state.focused ? 'focused iconfont' : 'iconfont' }>&#xe64d;</i>
+                    </SearchWrapper>
+                </Nav>
+                <Addition>
+                    <Button className='writting'>
+                        <i className='iconfont'>&#xe604;</i>
+                        写文章
+                    </Button>
+                    <Button className='reg'>注册</Button>
+                </Addition>
+            </HeaderWrapper>
+        )
+    }
+    handleInputFocus() {
+        this.setState({
+            focused: true,
+        })
+    }
+    handleInputBlur() {
+        this.setState({
+            focused: false,
+        })
+    }
+}
+
+export default Header;
